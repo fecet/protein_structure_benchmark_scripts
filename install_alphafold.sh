3724bb6fbd1736e6c081f9465ba38e877490f3f4
fecet <xiezej@gmail.com>
2022-08-17 15:54:28 +0800

    fix

--- install_alphafold.sh
+++ install_alphafold.sh
@@ -10,7 +10,6 @@ which python
 conda install -y -c conda-forge openmm==7.5.1 cudnn==8.2.1.32 cudatoolkit==11.1.1 pdbfixer==1.7
 conda install -y -c bioconda hmmer==3.3.2 hhsuite==3.3.0 kalign2==2.04
 
-# pip install --upgrade jax==0.2.14 jaxlib==0.1.69+cuda111 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 wget "https://github.com/deepmind/alphafold/archive/refs/tags/v2.2.0.tar.gz" && tar -xzf v2.2.0.tar.gz
 
@@ -22,6 +21,8 @@ export ALPHAFOLD_PATH="$(pwd)/alphafold"
 # conda activate "$CURRENT_ENV"
 
 pip install -r "$ALPHAFOLD_PATH/requirements.txt"
+
+pip install --upgrade jax==0.2.14 jaxlib==0.1.69+cuda111 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 wget -P "$ALPHAFOLD_PATH/alphafold/common/" "https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt"
 
