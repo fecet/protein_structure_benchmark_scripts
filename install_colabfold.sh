abb7282fd5e393516512d733ac04043f29a4b5dd
fecet <xiezej@gmail.com>
2022-08-20 11:38:23 +0800

    upd

--- install_colabfold.sh
+++ install_colabfold.sh
@@ -20,6 +20,6 @@ sh ./install_mmseqs2.sh
 
 ln -sf $(pwd)/MMseqs2/build/bin/mmseqs $CONDA_PREFIX/bin/mmseqs
 
-# ./setup_databases.sh
+# sh ./setup_databases.sh
 
 # colabfold_batch <directory_with_fasta_files> <result_dir> 
